1482d23c5f9011035d98c8749f07dd750ecfe7bf
Miriam Leffler <lefflerm@chc.edu>
2016-02-11 13:24:52 -0500

    Adding functionality to AddItem and the DB

--- app/src/main/java/com/example/administrator/shopper2/DBHandler.java
+++ app/src/main/java/com/example/administrator/shopper2/DBHandler.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by Administrator on 1/28/2016.
  */
 public class DBHandler extends SQLiteOpenHelper{
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
     private static final String DATABASE_NAME = "shopper.db";
 
 
@@ -20,23 +20,42 @@ public class DBHandler extends SQLiteOpenHelper{
     public static final String COLUMN_LIST_STORE = "list_store";
     public static final String COLUMN_LIST_DATE = "list_date";
 
+    public static final String TABLE_SHOPPING_LIST_ITEM = "shoppinglistitem";
+    public static final String COLUMN_ITEM_ID = "_id";
+    public static final String COLUMN_ITEM_NAME = "item_name";
+    public static final String COLUMN_ITEM_PRICE = "item_price";
+    public static final String COLUMN_ITEM_QUANTITY = "item_quantity";
+    public static final String COLUMN_ITEM_HAS = "item_has";
+    public static final String COLUMN_ITEM_LIST_ID = "item_list_id";
+
     public DBHandler(Context context, SQLiteDatabase.CursorFactory factory){
         super(context, DATABASE_NAME, factory, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query = "CREATE TABLE " + TABLE_SHOPPING_LIST + "(" +
+        String query = "CREATE TABLE " + TABLE_SHOPPING_LIST_ITEM + "(" +
+                COLUMN_ITEM_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_ITEM_NAME + " TEXT," +
+                COLUMN_ITEM_PRICE + " DECIMAL(10,2)," +
+                COLUMN_ITEM_QUANTITY + " INTEGER," +
+                COLUMN_ITEM_HAS + " TEXT," +
+                COLUMN_ITEM_LIST_ID + " INTEGER" +
+                ");";
+        db.execSQL(query);
+
+        String query2 = "CREATE TABLE " + TABLE_SHOPPING_LIST + "(" +
                 COLUMN_LIST_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 COLUMN_LIST_NAME + " TEXT," +
                 COLUMN_LIST_STORE + " TEXT," +
                 COLUMN_LIST_DATE + " TEXT" +
                 ");";
-        db.execSQL(query);
+        db.execSQL(query2);
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_SHOPPING_LIST);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SHOPPING_LIST_ITEM);
         onCreate(db);
     }
 
@@ -50,6 +69,19 @@ public class DBHandler extends SQLiteOpenHelper{
 
         db.insert(TABLE_SHOPPING_LIST, null, values);
 
+        db.close();
+    }
+
+    public void addItemToList(String name, Double price, Integer quantity, Integer listID){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_ITEM_NAME, name);
+        values.put(COLUMN_ITEM_PRICE, price);
+        values.put(COLUMN_ITEM_QUANTITY, quantity);
+        values.put(COLUMN_ITEM_HAS, "false");
+        values.put(COLUMN_ITEM_LIST_ID, listID);
+
+        db.insert(TABLE_SHOPPING_LIST_ITEM, null, values);
         db.close();
     }
 
